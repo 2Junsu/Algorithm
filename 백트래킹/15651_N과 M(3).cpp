23602099364dd9fab5cbe5536a399e3eb783de92
LeeJunsu <77619905+2Junsu@users.noreply.github.com>
2022-04-07 21:47:08 +0900

    Create 15651_N과 M(3).cpp

---
+++ 백트래킹/15651_N과 M(3).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int N, M;
+int arr[8];
+
+void dfs(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 1; i <= N; i++)
+		{
+			arr[depth] = i;
+			dfs(depth + 1);
+		}
+	}
+} 
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> M;
+
+	dfs(0);
+	return 0;
+}
